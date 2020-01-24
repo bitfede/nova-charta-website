31475a3a331ba55540aa90e217e25b9f9857b869
Fede Defaveri <f.g.defaveri@gmail.com>
2020-01-24 19:24:44 +0100

    Adds and writes the boilerplate code for the Header component

---
+++ components/Header/Header.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <div className="headerContainer">
+      <h1 className={styles.headerTitle}>HEADER</h1>
+    </div>
+  )
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,20 @@
 import React from 'react'
 // import fetch from 'isomorphic-unfetch'
 
+import Header from '../components/Header/Header'
+
 class HomePage extends React.Component {
   static async getInitialProps(ctx) {
     // const res = await fetch('https://api.github.com/repos/zeit/next.js')
     // const json = await res.json()
-    return { test: "yes" }
+    return { test: "yesss" }
   }
 
   render() {
     return (
-      <div>
-        <h1>HELLO WORLD</h1>
+      <div className="mainContainer">
+        <Header />
+        <h1>HELLO WORLD {this.props.test}</h1>
       </div>
   )
   }
