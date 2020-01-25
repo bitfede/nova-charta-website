27a48f0896ec5ba732991ef355abec917baabd59
Fede Defaveri <f.g.defaveri@gmail.com>
2020-01-25 18:45:14 +0100

    Adds the static image

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,7 +2,7 @@ import styles from './Header.module.css';
 
 function Header() {
   return (
-    <div className="headerContainer">
+    <div className={styles.headerContainer}>
       <h1 className={styles.headerTitle}>HEADER</h1>
     </div>
   )
