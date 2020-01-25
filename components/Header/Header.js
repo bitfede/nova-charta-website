6e09964244c8670ee888c6cee084ae6f9f108d01
bitfede <f.g.defaveri@gmail.com>
2020-01-25 22:27:39 +0100

    Puts logo and adds responsive breakpoints

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,15 +3,15 @@ import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstr
 
 function Header() {
   return (
-    <div className="headerContainer">
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home"><img src="../../static/img/carta.png" /></Navbar.Brand>
+    <div className={styles.headerContainer}>
+    <Navbar bg="" expand="lg">
+      <Navbar.Brand href="#home"><img className={styles.logoNavbar} src="/img/novacharta-logo.svg" /></Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown className={styles.dropdowns} title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
@@ -19,10 +19,6 @@ function Header() {
             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
           </NavDropdown>
         </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
       </Navbar.Collapse>
       </Navbar>
     </div>
