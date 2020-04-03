3a4764fd31f983917d96a65a6bbfb1738ca6b34f
bitfede <f.g.defaveri@gmail.com>
2020-04-03 16:49:50 -0400

    Renames a variable properly

--- components/Contatti/Contatti.js
+++ components/Contatti/Contatti.js
@@ -4,7 +4,7 @@ import { Container, Row, Col, Image, Form, Button  } from 'react-bootstrap'
 function Contatti(props) {
 
   return (
-    <div className={styles.corsiContainer}>
+    <div className={styles.contattiContainer}>
       <h1 className={styles.mainTitle}>Contatti</h1>
       <div dangerouslySetInnerHTML={{__html: props.contenutoPagina.content.rendered}}>
 
